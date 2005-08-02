9417ef2b73fc63bf1533b632c3ada2233877593c
phil <phil@caa26719-b0f5-0310-8df2-df1d84d3d8ee>
2005-08-02 22:32:35 +0000

    * /code/hylt: * /code/hylt/trunk:    Initial commit of Hylt infrastructure.
    * /code/hylt/trunk/hylt.py:
       Useless but nevertheless existing first commit of Hylt code.

---
+++ code/hylt/trunk/hylt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+##########
+#
+# hylt v.200508021730
+#
+# copyleft 2005 phil bordelon
+#
+##########
+
+import curses
+import curses.wrapper
+
+if "__main__" == __name__:
+   print "Hello!"
